2fb39ea9007da25bb9d76776fe694777eceac703
dartanham <phanton3001@gmail.com>
2021-11-24 18:17:29 -0300

    first commit

---
+++ src/com/teste/PrimeiroProjeto.java
@@ -0,0 +1,20 @@
+package com.teste;
+
+import com.teste.model.Gato;
+
+import java.sql.SQLOutput;
+
+public class PrimeiroProjeto {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World!! " + (a+b));*/
+    }
+}
